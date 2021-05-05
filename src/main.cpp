b9383bad5e284465ce02f05749f3fa9a927afb88
Bertrand LE GAL <bertrand.legal@ims-bordeaux.fr>
2021-05-05 17:30:17 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -163,7 +163,7 @@ int main(int argc, char* argv[])
 	float_t_show( B );
 
     std::complex<float> otherCplx[4];
-    const __m256 c = _mm256_set_ps(1.0f, 2.0f, 1.0f, 2.0f, 1.0f, 2.0f, 1.0f, 2.0f);
+    const __m256 c = _mm256_set_ps(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f);
     _mm256_storeu_ps ((float*)otherCplx, c);
 
 	vector< complex<float> > C;
